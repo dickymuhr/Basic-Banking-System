60fb5a3907ad37a2fd57a17448935eca39e5c3c8
Dicky Muhammad Rizal <dickymuhrizal@gmail.com>
2023-11-07 23:11:38 +0700

    add auth

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,9 +1,11 @@
 const users = require('./api/v1/users');
 const accounts = require('./api/v1/accounts');
 const transactions = require('./api/v1/transactions');
+const auth = require('./api/v1/auth');
 
 module.exports = {
     users,
     accounts,
-    transactions
+    transactions,
+    auth
 }
