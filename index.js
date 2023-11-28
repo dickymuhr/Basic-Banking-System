85848b753e470c072460ff4245cbfcb39412b703
Dicky Muhammad Rizal <dickymuhrizal@gmail.com>
2023-11-28 19:55:30 +0700

    add morgan & sentry

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 const express = require('express')
+const morgan = require('morgan')
+const Sentry = require("@sentry/node")
+const { ProfilingIntegration } = require("@sentry/profiling-node")
+
 const app = express()
 const routers = require('./router')
+const { SENTRY_DSN } = process.env
+
+Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({ app }),
+      new ProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+  
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(express.json())
 
+app.use(morgan('combined'))
+
 app.use(routers);
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+app.use(Sentry.Handlers.errorHandler());
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+    });
 
 module.exports = app; 
